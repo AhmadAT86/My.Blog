fa0d8aedb38b33e08b2167135b6cee7df077cabb
AhmadAT86 <ah.aboutaouk@gmail.com>
2016-01-25 13:54:28 +0100

    Test överflyttnig av bloggen

---
+++ My.Blog.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="My.Blog.Web.MvcApplication" Language="C#" %>
---
+++ My.Blog.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(My.Blog.Web.Startup))]
+namespace My.Blog.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
